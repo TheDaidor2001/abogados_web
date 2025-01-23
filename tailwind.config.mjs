50ea56ef9446535e493df4fbcdb3f4fa02c3230e
Daniel Castillo <danielcastillobalboa@gmail.com>
2025-01-23 10:52:06 +0100

    feat: add sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/flowbite/**/*.js'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+			heebo: ["Heebo", "sans-serif"],
+			maitree: ["Maitree", "serif"],
+			overpass: ["Overpass Variable", "sans-serif"],
+      },
+		},
 	},
 	plugins: [require('flowbite/plugin')],
 }
